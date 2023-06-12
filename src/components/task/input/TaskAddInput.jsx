471108d98d6d49fc864a9245aa8fc752a6836af0
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-12 22:42:21 +0900

    uuidの設定

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { v4 as uuid } from "uuid";
 
 export const TaskAddInput = ({
   inputText,
@@ -6,6 +7,7 @@ export const TaskAddInput = ({
   setTaskList,
   taskList}) => {
   const handleSubmit = (e) => {
+    const taskId = uuid()
     e.preventDefault();
     if (inputText === ""){
       return 
@@ -13,8 +15,8 @@ export const TaskAddInput = ({
     setTaskList([
       ...taskList,
       {
-        id: taskList.length,
-        draggableId: `task-${taskList.length}`,
+        id: taskId,
+        draggableId: `task-${taskId}`,
         text: inputText,
       }
     ])
