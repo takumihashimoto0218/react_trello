d7831a0d3673cb1d86d005505065e9e1270bf4bb
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-12 22:29:35 +0900

    並び替えの調整

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { Task } from "./Task";
 
-export const Tasks = ({ taskList, setTaskList} ) => {
+const reorder = (taskList, startIndex, endIndex) => {
+  //タスクを並び変える。
+  const remove = taskList.splice(startIndex, 1); //[2,3]
+  taskList.splice(endIndex, 0, remove[0]); //[2,1,3]
+};
+
+export const Tasks = ({ taskList, setTaskList }) => {
+  const handleDrangEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index);
+
+    setTaskList(taskList);
+  };
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleDrangEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef }>  
-              {taskList.map((task) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {taskList.map((task, index) => (
                 <div key={task.id}>
                   <Task
+                    index={index}
                     task={task}
                     taskList={taskList}
                     setTaskList={setTaskList}
                   />
                 </div>
-            ))}
-            {provided.placeholder}
-        </div>
-        )}
-      </Droppable>
-     </DragDropContext>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
-  )
-}
+  );
+};
