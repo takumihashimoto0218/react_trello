5effd8000d4c0a9d7b4f40d27665f10026b8270d
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-06-16 01:23:09 +0900

    タスクカードの削除を実装する

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -4,14 +4,14 @@ import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
 import { TaskAddInput } from "./input/TaskAddInput";
 import { Tasks } from "./Tasks";
 
-export const TaskCard = () => {
+export const TaskCard = ({taskCardsList, setTaskCardsList, taskCard}) => {
   const [inputText, setInputText] = useState("");
   const [taskList, setTaskList] = useState([]);
   return (
     <div className="taskCard">
       <div className="taskCardTitleAndTaskCardDeleteButtonArea">
         <TaskCardTitle />
-        <TaskCardDeleteButton />
+        <TaskCardDeleteButton taskCardsList={taskCardsList} setTaskCardsList={setTaskCardsList} taskCard={taskCard} />
       </div>
       <TaskAddInput 
         inputText={inputText} 
